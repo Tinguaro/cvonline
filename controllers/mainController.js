da8ded62d9eea34732857e0330f531a3fd42564c
Tinguaro Cubas Saiz <tinguarocubas@gmail.com>
2015-11-03 15:06:08 +0000

    primera pagina commit

---
+++ controllers/mainController.js
@@ -0,0 +1,107 @@
+function MainController($scope){
+
+
+	$scope.presentation = {
+		'urlImage' : 'img/perfil.jpg',
+		'name' : 'Tinguaro',
+		'subname' : 'Cubas Saiz',
+		'study' : 'Graduado en Ingeniería Informática',
+		'text' : '<< Cada día me miro en el espejo y me pregunto: "Si hoy fuese el último día de mi vida, ¿querría hacer lo que voy a hacer hoy?". Si la respuesta es "no" durante demasiados días seguidos, sé que necesito cambiar algo >>',
+		'nameText' : '- Steve Jobs.'
+	};
+
+	$scope.concursos = [
+		{ 	'img' : '',
+			'name' : 'OpenDataCanarias',
+			'text' : '',
+			'url' : 'http://www.opendatacanarias.es/apps/tenerife-tourist-guide'
+		},
+		{ 	'img' : '',
+			'name' : 'BigDataChallenge Telecom Italia',
+			'text' : '',
+			'url' : 'https://www.youtube.com/watch?v=8kLdIKfKhh8'
+		},
+		{ 	'img' : '',
+			'name' : 'Startup Weekend',
+			'text' : '',
+			'url' : 'https://www.facebook.com/proadvisorpage/'
+		}
+	];
+
+	$scope.conocimientos = [
+		{
+			'img' : '',
+			'name' : 'C'
+		},
+		{
+			'img' : '',
+			'name' : 'C++'
+		},
+		{
+			'img' : '',
+			'name' : 'Java'
+		},
+		{
+			'img' : '',
+			'name' : 'JavaScript'
+		},
+		{
+			'img' : '',
+			'name' : 'CSS'
+		},
+		{
+			'img' : '',
+			'name' : 'HTML'
+		},
+		{
+			'img' : '',
+			'name' : 'AngularJS'
+		},
+		{
+			'img' : '',
+			'name' : 'Node.js'
+		},
+		{
+			'img' : '',
+			'name' : 'MongoDB'
+		},
+		{
+			'img' : '',
+			'name' : 'SQL'
+		},
+		{
+			'img' : '',
+			'name' : 'Unity 3D'
+		},
+		{
+			'img' : '',
+			'name' : 'C#'
+		},
+		{
+			'img' : '',
+			'name' : 'PHP'
+		}
+	];
+
+
+	$scope.contactos = [
+		{
+			'img' : 'img/google.png',
+			'name' : 'tinguarocubas@gmail.com'
+		},
+		{
+			'img' : 'img/twitter.png',
+			'name' : '@TinguaroCS'
+		},
+		{
+			'img' : 'img/link.png',
+			'name' : 'Tinguaro Cubas Saiz'
+		},
+	];
+
+	$scope.cambiarIdioma = function (){
+		$scope.template.words = $scope.e;
+	};
+
+
+}
